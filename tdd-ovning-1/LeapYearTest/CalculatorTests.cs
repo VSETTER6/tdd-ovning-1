5357251726e8211c3b922b9509cdcc41411d9605
VSETTER6 <vilhelm.settergren@live.se>
2024-11-13 22:13:55 +0100

    First commit

---
+++ tdd-ovning-1/LeapYearTest/CalculatorTests.cs
@@ -0,0 +1,17 @@
+namespace LeapYearTest
+{
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
